933bb11a2335ef37da267a95c473e4ffccdcf3ed
Anandhaprakashs <harianandhpa@gmail.com>
2017-07-05 09:38:09 +0530

    Create fib.java

---
+++ fib.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class fib
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int a=0,b=1,cou=0;;
+		int n=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		    cou=a+b;
+		    System.out.print(cou+" ");
+		    a=b;
+		    b=cou;
+		}
+	}
+}
